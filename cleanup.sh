3a5c219e91df91432ddf51f12e904faec9c55754
WheeskyJack <32392032+WheeskyJack@users.noreply.github.com>
2018-09-11 10:36:32 +0530

    adding yum clean all to reduce the image size

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,6 @@ set -x
 
 find /bd_build/ -not \( -name 'bd_build' -or -name 'buildconfig' -or -name 'cleanup.sh' \) -delete
 rm -rf /tmp/* /var/tmp/*
-
+yum clean all && rm -rf /var/cache/yum
 ## removing proxy from yum.conf
 #sed -i -e '/^[[:space:]]*proxy=http:.*10.144.1.10:8080[[:space:]]*$/d' /etc/yum.conf
